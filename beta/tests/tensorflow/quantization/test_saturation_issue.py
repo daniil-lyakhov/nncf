34f295684c5af63420b37c8f3d11551cd13dc3c3
Daniil Lyakhov <daniil.lyakhov@intel.com>
2021-05-07 13:20:40 +0300

    Check round argument / confirm float arithmetic error

--- beta/tests/tensorflow/quantization/test_saturation_issue.py
+++ beta/tests/tensorflow/quantization/test_saturation_issue.py
@@ -58,11 +58,14 @@ TEST_MODELS = {
 def check_quantized_values_equals(y_train, y_val, eps, range_len, narrow_range):
     diff = np.abs(y_val - y_train)
     if np.max(diff) > eps:
-        # If any point gets in really close to the middle of quant
+        # If any point gets in really close to the middle of the quant
         # it can changes its quant due to rounding error
-        outlayers = diff[diff > 1e-6]
+        outlayers = diff[diff > eps]
         quant_len = range_len / (128 - (2 if narrow_range else 1))
-        assert np.max(np.abs(outlayers - quant_len)) < eps
+        assert (np.abs(outlayers - quant_len) < eps).all(), 'Quants are completely different'
+        assert False, 'Some values moved to the neighbor quant, possibly due to this values gets in ' \
+                      'really close to the middle of the quant. ' \
+                      f'Position of values: {np.where(diff > eps)[0].tolist()}'
 
 
 def check_quantized_values_clipped_correctly(y_val, low, range_len, eps, narrow_range):
@@ -87,6 +90,56 @@ def check_quantized_values_clipped_correctly(y_val, low, range_len, eps, narrow_
 def test_min_adj(bits, low, range_, narrow_range, ref):
     res = Quantizer._min_adj(bits, low, range_, narrow_range).numpy()# pylint: disable=protected-access
     assert abs(res - ref) < EPS
+
+
+@pytest.mark.parametrize('narrow_range', [False, True], ids=['full_range', 'narrow_range'])
+def test_round_arg_for_fq_assym_with_zero(narrow_range):
+    def round_arg(bits, x, min_adj, range, narrow_range):
+        return np.float32((np.float32(2 ** bits) - (2 if narrow_range else 1)) * (x - min_adj) / range)
+
+    eps = np.float32(1e-16)
+    low_int7 = np.float32(-1.)
+    range_int7 = np.float32(3. + eps)
+    quant_len = np.float32(range_int7 / (128 - (2 if narrow_range else 1)))
+    min_adj_int7 = np.float32(Quantizer._min_adj(7, low_int7, range_int7, narrow_range).numpy())
+    x = np.float32(min_adj_int7 + (5 + 1/2) * quant_len)
+    round_arg_int7 = round_arg(7, x, min_adj_int7, range_int7, narrow_range)
+
+    if not narrow_range:
+        range_int8 = np.float32(254 / 127) * range_int7
+    else:
+        range_int8 = np.float32(127 / 63) * range_int7
+
+    min_adj_int8 = low_int7 + min_adj_int7 # np.float32(Quantizer._min_adj(8, low_int7 + min_adj_int7, range_int8, True).numpy())
+    round_arg_int8 = round_arg(8, x, min_adj_int8, range_int8, narrow_range)
+    # Dequantize
+    x_dq_int7 = round(round_arg_int7) * quant_len + min_adj_int7
+    x_dq_int8 = round(round_arg_int8) * quant_len + min_adj_int8
+    if np.abs(np.abs(x_dq_int7 - x_dq_int8) - quant_len) < 1e-6:
+        assert False, "quant moved!"
+
+    assert np.abs(x_dq_int7 - x_dq_int8) < 1e-6
+
+
+@pytest.mark.parametrize('sign', [-1., 1.], ids=['less_zero', 'greater_zero'])
+@pytest.mark.parametrize('narrow_range', [False, True], ids=['full_range', 'narrow_range'])
+def test_round_arg_for_fq_assym_without_zero(sign, narrow_range):
+    def round_arg(bits, x, min_adj, range, narrow_range):
+        return np.float32((np.float32(2 ** bits) - (2 if narrow_range else 1)) * (x - min_adj) / range)
+
+    eps = np.float32(1e-16)
+    range_int7 = np.float32(3. + eps)
+    quant_len = np.float32(range_int7 / (128 - (2 if narrow_range else 1)))
+    x = np.float32(0. + sign * quant_len / 2)
+    round_arg_int7 = round_arg(7, x, range_int7, narrow_range)
+
+    if not narrow_range:
+        range_int8 = np.float32(255 / 127) * range_int7
+    else:
+        range_int8 = np.float32(127 / 63) * range_int7
+
+    round_arg_int8 = round_arg(8, x, range_int8, narrow_range)
+    assert np.abs(round_arg_int8 - round_arg_int7) < 1e-6
 
 
 @pytest.mark.parametrize('per_ch', [False, True], ids=['per_tensor', 'per_channel'])
@@ -128,7 +181,6 @@ def test_asymmetric_quantize_equal_on_train_and_val_per_tensor(low, range_len, n
     qconfig = QuantizerConfig(
         num_bits=8,
         mode=QuantizationMode.ASYMMETRIC,
-        signedness_to_force=None,
         per_channel=False)
     qspec = TFQuantizerSpec.from_config(
         qconfig,
@@ -155,7 +207,6 @@ def test_assym_quantize_equal_on_train_and_val_per_ch(shape, narrow_range):
     qconfig = QuantizerConfig(
         num_bits=8,
         mode=QuantizationMode.ASYMMETRIC,
-        signedness_to_force=None,
         per_channel=True)
     qspec = TFQuantizerSpec.from_config(
         qconfig,
@@ -188,6 +239,47 @@ def test_assym_quantize_equal_on_train_and_val_per_ch(shape, narrow_range):
     check_quantized_values_clipped_correctly(y_val[DIM_SPLIT:2*DIM_SPLIT], -5., range_len, EPS, narrow_range)
     # Range greater than zero
     check_quantized_values_clipped_correctly(y_val[2*DIM_SPLIT:], 3., range_len, EPS, narrow_range)
+
+
+@pytest.mark.parametrize('shape', [(128,), (1, 128), (1, 1, 1, 128)])
+@pytest.mark.parametrize('low,range_len', [(-1., 2.), (-5., 4.), (3., 2.)])
+@pytest.mark.parametrize('narrow_range', [False, True], ids=['full_range', 'narrow_range'])
+def test_assym_quantize_per_ch_takes_same_values_on_middle_quants_points(shape, low, range_len, narrow_range):
+    qconfig = QuantizerConfig(
+        num_bits=8,
+        mode=QuantizationMode.ASYMMETRIC,
+        per_channel=True)
+    qspec = TFQuantizerSpec.from_config(
+        qconfig,
+        narrow_range=narrow_range,
+        half_range=True)
+
+    quantizer_cls = NNCF_QUANTIZATION_OPERATONS.get(qspec.mode)
+    quantizer = quantizer_cls('quantizer', qspec)
+    weights = quantizer.build(shape, InputType.WEIGHTS, 'dummy_name', tf.keras.layers.Layer())
+
+    range_len = 3
+    low_t = tf.constant([low] * 128, dtype=tf.float32)
+    range_t = tf.constant([range_len] * 128, dtype=tf.float32)
+    weights['input_low_var'].assign(low_t)
+    weights['input_range_var'].assign(range_t)
+
+    quant_len = range_len / (128 - (2 if narrow_range else 1))
+    if low > EPS:
+        # Range greater than zero
+        x = tf.constant([(i + 1/2) * quant_len for i in range(128)], dtype=tf.float32)
+    elif low + range_len < EPS:
+        # Range lower than zero
+        x = tf.constant([-(i + 1/2) * quant_len for i in range(128)], dtype=tf.float32)
+    else:
+        # Range with zero
+        min_adj = Quantizer._min_adj(7., low, range_len, narrow_range).numpy()# pylint: disable=protected-access
+        x = tf.constant([min_adj + (i + 1/2) * quant_len for i in range(128)], dtype=tf.float32)
+
+    x = tf.reshape(x, shape)
+    y_train = quantizer(x, weights, training=True).numpy().flatten()
+    y_val = quantizer(x, weights, training=False).numpy().flatten()
+    check_quantized_values_equals(y_train, y_val, EPS, range_len, narrow_range)
 
 
 @pytest.mark.parametrize('model_cls', list(TEST_MODELS.values()), ids=list(TEST_MODELS.keys()))
