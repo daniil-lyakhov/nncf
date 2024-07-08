125a10f6f16cd51f14fe4ad66507af9997b67734
Aamir Nazir <aamirnazir299@gmail.com>
2024-07-08 18:56:18 +0400

    add test for mobilenet_v3_small and vit_b_16.

--- tests/torch/fx/test_models.py
+++ tests/torch/fx/test_models.py
@@ -42,26 +42,27 @@ def tiny_imagenet_dataset_fixture():
 @dataclass
 class ModelCase:
     model_id: str
-    checkpoint_url: str
 
 
 MODELS = (
     ModelCase(
-        "resnet18",
-        "https://storage.openvinotoolkit.org/repositories/nncf/openvino_notebook_ckpts/302_resnet18_fp32_v1.pth",
+        "resnet18"
+    ),
+    ModelCase(
+        "mobilenet_v3_small"
+    ),
+    ModelCase(
+        "vit_b_16"
     ),
 )
 
 
 def get_model(
-    model_id: str, checkpoint_url: str, device: torch.device, num_classes: int = 200, in_features: int = 512
+    model_id: str, device: torch.device
 ) -> torch.nn.Module:
     model = getattr(models, model_id)(weights=None)
-    # Update the last FC layer for Tiny ImageNet number of classes.
-    model.fc = nn.Linear(in_features=in_features, out_features=num_classes, bias=True)
     model.to(device)
-    checkpoint = torch.hub.load_state_dict_from_url(checkpoint_url, map_location=torch.device("cpu"), progress=False)
-    model.load_state_dict(checkpoint["state_dict"])
+
     return model
 
 
@@ -75,6 +76,7 @@ def getNodeType(node: torch.fx.node) -> str:
 def isNodeMetatype(node_type: str) -> bool:
     op_type = PT_OPERATOR_METATYPES.get_operator_metatype_by_op_name(node_type)
     if op_type is UnknownMetatype:
+        print(node_type)
         return False
     return True
 
@@ -85,20 +87,14 @@ def retrieve_nodes(model: torch.fx.GraphModule):
 
 
 @pytest.mark.parametrize("test_case", MODELS)
-def test_sanity(test_case: ModelCase, tiny_imagenet_dataset):
+def test_models(test_case: ModelCase, tiny_imagenet_dataset):
     with disable_patching():
         torch.manual_seed(42)
         device = torch.device("cpu")
-        model = get_model(test_case.model_id, test_case.checkpoint_url, device)
-        _, _, calibration_dataset = tiny_imagenet_dataset
-
-        def transform_fn(data_item):
-            return data_item[0].to(device)
-
-        calibration_dataset = nncf.Dataset(calibration_dataset, transform_fn)
+        model = get_model(test_case.model_id, device)
 
         with torch.no_grad():
-            ex_input = next(iter(calibration_dataset.get_inference_data()))
+            ex_input = torch.randn(1, 3, 224, 224)
             model.eval()
             exported_model = capture_pre_autograd_graph(model, args=(ex_input,))
             nodes = retrieve_nodes(exported_model)
